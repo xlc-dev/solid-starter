dcf834e496005d5cd256bdd2b5246d9a9540d500
xlc-dev <119954023+xlc-dev@users.noreply.github.com>
2025-06-02 10:16:04 +0200

    add icons to vite config

--- app.config.ts
+++ app.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "@solidjs/start/config";
 import tailwindcss from "@tailwindcss/vite";
 // @ts-expect-error Errors for no reason cuz node doesn't find it?
 import eslint from "vite-plugin-eslint";
+import Icons from "unplugin-icons/vite";
 
 export default defineConfig({
   middleware: "src/middleware.ts",
   vite: {
     plugins: [
+      Icons({ compiler: "solid" }),
       tailwindcss(),
       eslint({
         fix: true,
